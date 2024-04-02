a716b2a2130b6f0c56e7a8a2efeb02d72782ae07
michele-pupo <mikypupo@hotmail.it>
2024-04-02 17:43:16 +0200

    end exercise

--- index.php
+++ index.php
@@ -10,21 +10,32 @@ require 'db.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Movie</title>
+
+    <!-- bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
+    <!-- style -->
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-    <h1>Film</h1>
-    <ul>
-        <?php 
-            foreach($films as $film){
-                echo "
-                <li> 
-                    Titolo: ".$film->title." <br>
-                    Anno: ".$film->comingDate." <br>
-                    Regista: ".$film->director?->getDirector()." <br>
-                    Genere: " .$film->genre?->getGenre(). "
-                </li>";
-            }
-        ?>
-    </ul>
+    <div class="container">
+        <h1 class="fw-bold text-center pt-4">Film</h1>
+        <div class="d-flex flex-column align-items-center py-4">
+            <?php 
+                foreach($films as $film){
+                    echo "
+                        <h2> $film->title </h2>
+                        <h3> $film->comingDate </h3>
+                        <h4> " .$film->director?->getDirector(). " </h4>
+                        <h5> " .$film->genre?->getGenre(). "</h5>
+                    ";
+                }
+            ?>
+        </div>
+    </div>
+    
+
+    <!-- script bootstrap -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 </html>
