8043918303c418c799f36ceb23eee05982a4fbb2
michele-pupo <mikypupo@hotmail.it>
2024-04-02 15:54:20 +0200

    modified the movie class with 2 objects

--- index.php
+++ index.php
@@ -8,29 +8,56 @@ class Movie {
         public $director;
         public $genre;
 
-        // funzione costruttore
-        function __construct($_title, $_comingDate, $_director) {
+        // funzione costruttore        
+        /**
+         * __construct
+         *
+         * @param  string $_title
+         * @param  int $_comingDate
+         * @param  string $_director
+         */
+        function __construct($_title, $_comingDate, $_director, Genre $_genre) {
             // gli attributi della classe
             $this->title = $_title;
             $this->comingDate = $_comingDate;
             $this->director = $_director;
+            $this->genre = $_genre;
         }
     }
+class Genre {
+    public $genre1;
+    public $genre2;
+    
+    /**
+     * __construct
+     *
+     * @param  string $_genre1
+     * @param  string $_genre2
+     */
+    function __construct($_genre1, $_genre2) {
+        $this->genre1 = $_genre1;
+        $this->genre2 = $_genre2;
+    }
+}
+$film1Genre = new Genre("Romantico","Thriller");
 
-$film1 = new Movie("La teoria del tutto", 2014, "James Marsh");
+$film1 = new Movie("La teoria del tutto", 2014, "James Marsh", $film1Genre);
 
-$film1->genre = "Romantico";
-var_dump($film1);
+// var_dump($film1);
 
-$film2 = new Movie("A Beautiful Mind", 2001, "Ron Howard");
 
-$film2->genre = "Thriller";
-var_dump($film2);
+$film2Genre = new Genre("Trhiller","Guerra");
 
-$film3 = new Movie("Rush", 2013, "Ron Howard");
+$film2 = new Movie("A Beautiful Mind", 2001, "Ron Howard", $film2Genre);
 
-$film3->genre = "Sportivo";
-var_dump($film3);
+// var_dump($film2);
+
+
+$film3Genre = new Genre("Sportivo","Azione");
+
+$film3 = new Movie("Rush", 2013, "Ron Howard", $film3Genre);
+
+// var_dump($film3);
 
 
 // creo un array di film
