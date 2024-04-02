d9931da882d4b42ad368b992a47d26a0c1b5517b
michele-pupo <mikypupo@hotmail.it>
2024-04-02 14:34:49 +0200

    printed via var_dump film on page

--- index.php
+++ index.php
@@ -8,6 +8,28 @@ class Movie {
         public $director;
         public $genre;
     }
+
+$film1 = new Movie();
+// personalizziamo le proprietà
+$film1->title = "La Teoria Del Tutto";
+$film1->comingDate = "2014";
+$film1->director = "James Marsh";
+var_dump($film1);
+
+$film2 = new Movie();
+// personalizziamo le proprietà
+$film2->title = "A Beautiful Mind";
+$film2->comingDate = "2001";
+$film2->director = "Ron Howard";
+var_dump($film2);
+
+$film3 = new Movie();
+// personalizziamo le proprietà
+$film3->title = "Rush";
+$film3->comingDate = "2013";
+$film3->director = "Ron Howard";
+var_dump($film3);
+
 ?>
 
 <!DOCTYPE html>
